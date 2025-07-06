ab8a01c58876b23279dc47affe5af85e07d7074a
yy406 <myon_yy406@outlook.jp>
2025-07-06 23:32:10 +0900

    index.htmlにタイトルとセクションを追加し、script.jsで新しいテーブルを動的に生成する機能を実装

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
+const data1 = [
+    ["ID", "改修"],
+    ["A", 10],
+    ["B", 10],
+    ["C", 10],
+    ["D", 10],
+    ["E", 10],
+  ];
+
+// HTMLの要素に表を追加する
+var container = document.getElementById("table1");
+container.appendChild(createTable(data1));
+var container = document.getElementById("table2");
+container.appendChild(createTable(enumerateCombinations(data1)));
+
 // 表を作る関数
 function createTable(data) {
-const table = document.createElement("table");
-table.border = "1"; // 罫線をつける（簡易的に）
-
-data.forEach((rowData, rowIndex) => {
-    const row = document.createElement("tr");
-    rowData.forEach(cellData => {
-    const cell = rowIndex === 0 ? document.createElement("th") : document.createElement("td");
-    cell.textContent = cellData;
-    row.appendChild(cell);
+    const table = document.createElement("table");
+    table.border = "1"; // 罫線をつける（簡易的に）
+    
+    data.forEach((rowData, rowIndex) => {
+        const row = document.createElement("tr");
+        rowData.forEach(cellData => {
+        const cell = rowIndex === 0 ? document.createElement("th") : document.createElement("td");
+        cell.textContent = cellData;
+        row.appendChild(cell);
+        });
+        table.appendChild(row);
     });
-    table.appendChild(row);
-});
-
-return table;
+    
+    return table;
 }
 
+// 装備の組み合わせを列挙する関数
 function enumerateCombinations(data, maxSlots = 4) {
     let equipCounts = data.length - 1; // ヘッダーを除く
     let nHrCounts = nHr(equipCounts, maxSlots)
@@ -38,37 +54,14 @@ function enumerateCombinations(data, maxSlots = 4) {
           }
           result.push([...result_i]); // 配列のコピーを追加
     }
+    return result;
 }
 
 // 重複組み合わせ、n：装備種類数、r：スロット数
 function nHr(n, r) {
-  // 階乗を計算するための関数を定義
-  const factorial = num => (num <= 1) ? 1 : num * factorial(num - 1);
-  
-  // nHr の計算を行う式
-  return factorial(n + r - 1) / (factorial(n - 1) * factorial(r));
-}
-
-const data = [
-    ["ID", "改修"],
-    ["A", 10],
-    ["B", 10],
-    ["C", 10],
-    ["D", 10],
-    ["E", 10],
-  ];
-
-const data2 = [
-    ["名前", "年齢", "職業"],
-    ["山田太郎2", 30, "エンジニア"],
-    ["鈴木花子2", 25, "デザイナー"],
-  ];
-
-
-// HTMLの要素に表を追加する
-const container = document.getElementById("table");
-container.appendChild(createTable(data));  // createTableは表を作る関数
-const container2 = document.getElementById("table2");
-container2.appendChild(createTable(data2));  // createTableは表を作る関数
-const container3 = document.getElementById("table3");
-container3.appendChild(createTable(enumerateCombinations(data)));  // createTableは表を作る関数
+    // 階乗を計算するための関数を定義
+    const factorial = num => (num <= 1) ? 1 : num * factorial(num - 1);
+    
+    // nHr の計算を行う式
+    return factorial(n + r - 1) / (factorial(n - 1) * factorial(r));
+  }
